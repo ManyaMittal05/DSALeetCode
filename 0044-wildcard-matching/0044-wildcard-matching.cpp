d4c2ac10f0d43b26dd320fc0df727c08c621c7b9
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-17 16:52:28 +0530

    Time: 56 ms (41.09%), Space: 12.1 MB (82.88%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
 
-//Tabulation using memoization with 1 based indexing 
+//SPace Optimization using Tabulation
 //time - O(n*m)
-//space - O(n*m)
+//space - O(m)
     
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<vector<bool>> dp(n+1, vector<bool> (m+1, false));
+        vector<bool> prev(m+1, false), curr(m+1, false);
 
         //base cases of memoization in tabulation
         for(int j = 1; j <= m; j++){
@@ -18,23 +18,28 @@ public:
                     break;
                 }
             }
-            if(flag == 0) dp[0][j] = true;
+            if(flag == 0){
+                prev[j] = true;
+                curr[j] = true;
+            } 
         }
-        for(int i = 1; i<= n; i++){
-            dp[i][0] = false;
-        }
-        dp[0][0] = true;
+    
+        prev[0] = true;
+        curr[0] = true;
 
         //exploring all possibilities
         for(int i = 1; i<=n; i++){
+            curr[0] = false; //for every row for which i > 0, j = 0 in tabulation
             for(int j = 1; j<=m; j++){
                 if(s[i-1] == p[j-1] || p[j-1] == '?')
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 else if(p[j-1] == '*')
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    curr[j] = prev[j] || curr[j-1];
+                else curr[j] = false;
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return curr[m];
     }
 };
