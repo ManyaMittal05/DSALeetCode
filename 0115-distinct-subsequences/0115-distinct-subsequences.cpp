dbf381511461270fd1ccba269c10896f707e338a
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-17 15:32:49 +0530

    Time: 13 ms (80.76%), Space: 8 MB (84.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-//Tabulation using 1 based indexing
+//SPace Optimization using 1 based indexing
 //time - O(n*m)
-//space - O(n*m) 
+//space - O(m) 
 
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<double>> dp(n+1, vector<double> (m+1, 0));
+        vector<double> prev(m+1, 0), curr(m+1, 0);
 
         //base cases of memoization
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 1;
-        }
+        prev[0] = 1, curr[0] = 1;
         //no need to write below base case as we have have already intialized array with 0
         // for(int j = 1; j <= m; j++){ //from j = 1 as dp[0][0] = 1
-        //     dp[0][j] = 0;
+        //     prev[j] = 0;
         // }
 
         //exploring all possibilities
         for(int i = 1; i<= n; i++){
             for(int j = 1; j<=m; j++){
                 if(s[i-1] == t[j-1])
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 else
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
             }
+            prev = curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
