c86ff1bef4d17bb9777ee61aa07ba6343cbc7692
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-18 09:13:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (11.72%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count = 0, ans = 0;
+        for(auto ch : s){
+            if(ch == '('){
+                count++;
+                ans = max(ans, count);
+            }
+            else if(ch == ')'){
+                count--;
+            }
+        }
+        return ans;
+    }
+};
