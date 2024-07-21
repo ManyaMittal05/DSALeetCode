90669f499ce5b2dba5637bb0418aab149c11d83c
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-21 11:56:25 +0530

    Time: 19 ms (37.39%), Space: 22 MB (13.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,12 +4,9 @@ public:
         int j = 0;
         for(int i = 0; i<nums.size(); i++){
             if(nums[i] != 0){
-                nums[j] = nums[i];
+                swap(nums[j], nums[i]);
                 j++;
             }
-        }
-        for(int k = j; k < nums.size(); k++){
-            nums[k] = 0;
         }
     }
 };
