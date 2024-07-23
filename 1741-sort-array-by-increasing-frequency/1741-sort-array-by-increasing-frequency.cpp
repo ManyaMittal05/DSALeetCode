64899d79824fe41d6ca344739687529920a9b469
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-23 08:10:36 +0530

    Time: 4 ms (73.1%), Space: 14.7 MB (70.42%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        map<int,int> mp;
-        for(int n : nums){
-            mp[n]++;
+        vector<int> freq(201, 0);
+        for(auto n : nums){
+            freq[n + 100]++;
         }
 
-        vector<pair<int, int>> vec;
-        for(auto [val ,count] : mp){
-            vec.push_back({val , count});
-        }
-
-        sort(vec.begin(), vec.end(), [&](pair<int,int> a, pair<int,int> b){
-            if(a.second < b.second) return true;
-            else if(a.second > b.second) return false;
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            if(freq[100 + a] < freq[100 + b]) return true;
+            else if(freq[100 + a] > freq[100 + b]) return false;
             else{
-                if(a.first > b.first) return true;
+                if(a > b) return true;
                 return false;
             }
         });
-
-        int i = 0;
-        for(int j = 0; j < vec.size(); j++){
-            for(int k = 0; k<vec[j].second; k++){
-                nums[i] = vec[j].first;
-                i++;
-            }
-        }
 
         return nums;
     }
