7fe7e1a90e3974295c27cb38cbd2887f979d46ae
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-28 19:27:33 +0530

    Time: 7 ms (81.36%), Space: 18.6 MB (39.77%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //using hash map
+        //at max there can be two elements in the nums whose frequency is > n/3
+        //so initialise two counters and two elements for ans
         int n = nums.size();
-        int maj = n / 3 + 1;
-        unordered_map<int,int> mp;
-        vector<int> ans;
-        for(int i = 0; i<nums.size(); i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]] == maj)
-                ans.push_back(nums[i]);
-            if(ans.size() == 2) break;
+        int count1 = 0, count2 = 0;
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        for(int i = 0; i<n; i++){
+            if(count1  == 0 && ele2 != nums[i]){
+                count1 = 1;
+                ele1 = nums[i];
+            }
+            else if(count2 == 0 && ele1 != nums[i]){
+                count2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1) count1++;
+            else if(nums[i] == ele2) count2++;
+            else{
+                count1--;
+                count2--;
+            }
         }
-        
+
+        //validate if the ele1 and ele2 frequnecy is greater than n/3
+        int maj = n/3;
+        count1 = 0, count2 = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] == ele1) count1++;
+            if(nums[i] == ele2) count2++;
+        }
+        vector<int> ans;
+        if(count1 > maj) ans.push_back(ele1);
+        if(count2 > maj) ans.push_back(ele2);
         return ans;
     }
 };
