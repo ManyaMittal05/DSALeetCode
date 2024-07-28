85a6373d66c6754f9bd7342398a07819bc3d8268
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-28 18:52:56 +0530

    Time: 18 ms (7.28%), Space: 19.9 MB (6.29%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,18 +3,16 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         //using hash map
         int n = nums.size();
-        int maj = n / 3;
+        int maj = n / 3 + 1;
         unordered_map<int,int> mp;
         vector<int> ans;
         for(int i = 0; i<nums.size(); i++){
             mp[nums[i]]++;
-        }
-
-        for(auto [n , count] : mp){
-            if(count > maj)
-                ans.push_back(n);
+            if(mp[nums[i]] == maj)
+                ans.push_back(nums[i]);
             if(ans.size() == 2) break;
         }
+        
         return ans;
     }
 };
