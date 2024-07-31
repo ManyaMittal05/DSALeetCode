2e01e5579f52dec29c9b0086ab72eadf8b88fa53
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-07-31 17:45:05 +0530

    Time: 12 ms (9.7%), Space: 20.9 MB (14.03%) - LeetHub

--- 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int shelfWidth, n;
-    int dp[1000]; // dp[i] height for book i
+//memoization
 
-    int f(int i, vector<vector<int>>& books) {
-        if (i<0) return 0; // Base case: no more books to place
-        if (dp[i]!= -1) return dp[i]; 
-        int layerH=0, Width_rem=shelfWidth, ans=INT_MAX;
-        for(int j=i; j>=0; j--){
-            Width_rem-=books[j][0];
-            if (Width_rem<0) break;
-            layerH=max(layerH, books[j][1]);
-            ans=min(ans, layerH+f(j-1, books));
+    int solve(vector<vector<int>>& books, int ind, int remainingWidth, int maxHt,
+                vector<vector<int>>& dp, int shelfWidth){
+        if(ind < 0) return maxHt;
+        
+        if(dp[ind][remainingWidth] != -1) return dp[ind][remainingWidth];
+        int bookW = books[ind][0];
+        int bookHt = books[ind][1];
+
+        int keep = INT_MAX, skip = INT_MAX;
+
+        if(bookW <= remainingWidth){
+            keep = solve(books, ind-1, remainingWidth - bookW, max(maxHt, bookHt), dp, shelfWidth);
         }
-        return dp[i]=ans;
+        
+        skip = maxHt + solve(books, ind-1, shelfWidth - bookW, bookHt, dp, shelfWidth);
+    
+        return dp[ind][remainingWidth] = min(keep, skip);
     }
-
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        this->shelfWidth=shelfWidth;
-        n=books.size();
-        memset(dp, -1, sizeof(dp)); 
-        return f(n-1, books); // Start from the last book
+        int n = books.size();
+        vector<vector<int>> dp(n, vector<int> (shelfWidth+1, -1));
+        return solve(books, n-1, shelfWidth, 0, dp, shelfWidth);
+        
     }
 };
