6450d7bbffd1cfd028cb5e369decf101351fa66e
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-01 10:19:01 +0530

    Time: 8 ms (47.01%), Space: 17.5 MB (27.35%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -3,10 +3,8 @@ public:
     int countSeniors(vector<string>& details) {
         int count = 0;
         for(auto s : details){
-            int age = 0;
-            age += (s[11] - '0');
-            age = age * 10 + (s[12] - '0');
-            if(age > 60) 
+            string age = s.substr(11,2);
+            if(stoi(age) > 60)
                 count++;
         }
         return count;
