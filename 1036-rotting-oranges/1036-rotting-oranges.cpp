0bf178d82b089e4f8603819ac810aebab0178a1b
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-07 13:56:16 +0530

    Time: 14 ms (5.92%), Space: 18.9 MB (5.18%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> directions = {{1,0}, {0,-1}, {0,1}, {-1,0}};
+
+    //since we need to simulateously rotten the oranges which are present int the neighbourhood
+    //we will use the level wise order traversal i.e. bfs
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited = grid;
+        queue<vector<int>> q;
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 2){
+                    q.push({i, j, 0});
+                }
+            }
+        }
+        int max_time = 0;
+        while(!q.empty()){
+            vector<int> values = q.front();
+            q.pop();
+            int i = values[0];
+            int j = values[1];
+            int time = values[2];
+            for(auto d : directions){
+                int new_i = i + d[0];
+                int new_j = j + d[1];
+
+                if(new_i >= 0 && new_i < m && new_j >=0 && new_j < n 
+                        && visited[new_i][new_j] == 1){
+                    visited[new_i][new_j] = 2;
+                    q.push({new_i, new_j, time+1});
+                    max_time = max(max_time, time+1);
+                }
+            }
+        }
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(visited[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+        return max_time;
+    }
+};
