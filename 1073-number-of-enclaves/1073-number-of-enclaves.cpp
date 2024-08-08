80a6dddf00702e941d30603b9e39ff4cbde1591c
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-08 18:47:38 +0530

    Time: 107 ms (17.22%), Space: 43.8 MB (16.59%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
 //similar to surrounded region question
+//doing by bfs
     vector<vector<int>> dir = {{0,1}, {0,-1}, {1,0} , {-1,0}};
-
-    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& visited){
-        visited[i][j] = true;
-
-        for(auto d : dir){
-            int new_i = i + d[0];
-            int new_j = j + d[1];
-
-            if(new_i >= 0 && new_i < grid.size() && new_j >= 0 && new_j < grid[0].size()){
-                if(!visited[new_i][new_j] && grid[new_i][new_j] == 1){
-                    dfs(new_i, new_j, grid, visited);
-                }
-            }
-        }
-    }
 
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool> (n, false));
+        queue<pair<int,int>> q;
 
         //mark all the land boundary cells and the cells connected to it as visited
         //after marking them visited we will count all the non visited land cells
@@ -28,23 +15,46 @@ public:
 
         //checking the upper boundary row and lower boundary row
         for(int j = 0; j<n; j++){
-            if(grid[0][j] == 1 && !visited[0][j]){
-                dfs(0, j, grid, visited);
+            if(grid[0][j] == 1){
+                q.push({0,j});
+                visited[0][j] = true;
             }
 
-            if(grid[m-1][j] == 1 && !visited[m-1][j]){
-                dfs(m-1, j, grid, visited);
+            if(grid[m-1][j] == 1){
+                q.push({m-1, j});
+                visited[m-1][j] = true;
             }
         }
 
         //checing the left boundary column and right boundary column
         for(int i = 0; i < m; i++){
-            if(grid[i][0] == 1 && !visited[i][0]){
-                dfs(i, 0, grid, visited);
+            if(grid[i][0] == 1){
+                q.push({i, 0});
+                visited[i][0] = true;
             }
 
-            if(grid[i][n-1] == 1 && !visited[i][n-1]){
-                dfs(i, n-1, grid, visited);
+            if(grid[i][n-1] == 1){
+                q.push({i, n-1});
+                visited[i][n-1] = true;
+            }
+        }
+
+        while(!q.empty()){
+            pair<int,int> coord = q.front();
+            int i = coord.first;
+            int j = coord.second;
+            q.pop();
+
+            for(auto d : dir){
+                int new_i = i + d[0];
+                int new_j = j + d[1];
+
+                if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n ){
+                    if(grid[new_i][new_j] == 1 && !visited[new_i][new_j]){
+                        visited[new_i][new_j] = true;
+                        q.push({new_i, new_j});
+                    }
+                }
             }
         }
 
