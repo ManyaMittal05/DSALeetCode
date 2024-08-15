d6e67c13eb9fb4d5b0157ec613d66664c34f5975
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-15 17:12:44 +0530

    Time: 18 ms (48.08%), Space: 18.4 MB (41.18%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -20,21 +20,30 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         priority_queue<ListNode*, vector<ListNode*>, compare> minHeap;
         int k = lists.size();
+
+        //put the head of all the k lists into the minHeap
         for(int i = 0; i<k; i++){
             if(lists[i] != NULL)
                 minHeap.push(lists[i]);
         }
 
+        //make a new answer list that will store the result of the merged lists
+        //store the head of the linked list as 0
         ListNode* ans = new ListNode();
         ListNode* tmp = ans;
         while(!minHeap.empty()){
+            //extract the top node of the list and insert it int the ans list
             ListNode* node = minHeap.top();
             tmp->next = node;
             tmp = tmp->next;
+
+            //now move the node to next node of the list whose element is used
             node = node->next;
 
+            //pop the top node of the minHeap
             minHeap.pop();
 
+            //if node is not null then insert it into the list
             if(node != NULL){
                 minHeap.push(node);
             }
