210bdd7ec81c46426cbc488832f1433d5005d245
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-16 15:34:05 +0530

    Time: 218 ms (89.15%), Space: 107.9 MB (57.55%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        vector<pair<int, int>> mini, maxi;
-        int diff = INT_MIN;
-        for(int i=0; i<arrays.size(); i++){
-            mini.push_back({arrays[i][0], i});
-            maxi.push_back({arrays[i][arrays[i].size()-1], i});
+        int smallest = arrays[0][0];
+        int biggest = arrays[0].back();
+        int max_distance = 0;
+
+        for (int i = 1; i < arrays.size(); ++i) {
+            max_distance = max(max_distance, abs(arrays[i].back() - smallest));
+            max_distance = max(max_distance, abs(biggest - arrays[i][0]));
+            smallest = min(smallest, arrays[i][0]);
+            biggest = max(biggest, arrays[i].back());
         }
-        int n = arrays.size();
-        sort(mini.begin(), mini.end());
-        sort(maxi.begin(), maxi.end());
-        
-        if(mini[0].second != maxi[n-1].second){
-            diff = maxi[n-1].first - mini[0].first;
-        }
-        else{
-            int d1 = maxi[n-2].first - mini[0].first;
-            int d2 = maxi[n-1].first - mini[1].first;
-            diff = max(d1, d2);
-        }
-        return diff;
+
+        return max_distance;
     }
 };
