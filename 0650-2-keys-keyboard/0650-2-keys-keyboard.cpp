33232ab14e767d4a12a51299aea049f447066c95
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-19 21:45:26 +0530

    Time: 33 ms (11.38%), Space: 155.6 MB (16.17%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,7 +1,7 @@
 class Solution {
     //recursive solution
 public:
-    int solve(int n, int currA, int clipA) {
+    int solve(int n, int currA, int clipA, vector<vector<int>>& dp) {
         if (currA == n) {
             return 0;
         }
@@ -10,19 +10,22 @@ public:
             return 1e7;
         }
 
+        if(dp[currA][clipA] != -1) return dp[currA][clipA];
+
         // Copy and paste operation
-        int copyPaste = 1 + 1 + solve(n, currA + currA, currA);
+        int copyPaste = 1 + 1 + solve(n, currA + currA, currA, dp);
 
         //no copy, only paste
-        int paste = 1 + solve(n, currA + clipA, clipA);
+        int paste = 1 + solve(n, currA + clipA, clipA, dp);
 
-        return min(copyPaste, paste);
+        return dp[currA][clipA] = min(copyPaste, paste);
     }
 
     int minSteps(int n) {
         if(n == 1 ) return 0;
 
+        vector<vector<int>> dp(n, vector<int> (n, -1));
         int currA = 1, clipA = 1;
-        return 1 + solve(n, currA, clipA);
+        return 1 + solve(n, currA, clipA, dp);
     }
 };
