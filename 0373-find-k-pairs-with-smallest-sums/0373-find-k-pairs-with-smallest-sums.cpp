05ac53be4f58f5542358d5b0acff17e55bc58e33
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-20 15:37:49 +0530

    Time: 237 ms (23.41%), Space: 140.9 MB (53.02%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -20,26 +20,39 @@ public:
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
             compare> pq;
 
-        pq.push({nums1[0] + nums2[0], {0, 0}});
+        //first ele of both arrays will surely give a min sum as 
+        //they are the msallest elements coz the arrays are sorted
+        pq.push({nums1[0] + nums2[0], {0, 0}}); 
 
         while(k > 0 && !pq.empty()){
+            //pop out the top of the heap
+            //i.e. the min sum values and the indices from each array that give min sum
             pair<int, pair<int, int>> p = pq.top();
             pq.pop();
             pair<int, int> indices = p.second;
             int i = indices.first;
             int j = indices.second;
 
+            //push the values at the indices in the result array
             result.push_back({nums1[i], nums2[j]});
-            k--;
+            k--; //decrement the count of k as we need to find only k pairs
 
+            //for next possible pair i.e. (i+1, j) check if they are not out of bound
             if(i + 1 < nums1.size()){
+                //check if the new pair already exits in the min Heap
+                //if not then insert it into the set and the min heap
+                //along with the result so it can maintain min heap property
                 if(set.find({i+1, j}) == set.end()){
                     set.insert({i+1, j});
                     pq.push({nums1[i+1] + nums2[j], {i+1, j}});
                 }
             }
 
+            //for next possible pair i.e. (i, j+1) check if they are not out of bound
             if(j + 1 < nums2.size()){
+                //check if the new pair already exits in the min Heap
+                //if not then insert it into the set and the min heap
+                //along with the result so it can maintain min heap property
                 if(set.find({i, j+1}) == set.end()){
                     set.insert({i, j+1});
                     pq.push({nums1[i] + nums2[j+1], {i, j+1}});
