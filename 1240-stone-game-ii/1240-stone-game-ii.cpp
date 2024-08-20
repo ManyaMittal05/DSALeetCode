d1f6a575bb03ec981d0d7ed9a991c23da3e30b00
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-20 09:16:10 +0530

    Time: 24 ms (50.88%), Space: 15.7 MB (43.83%) - LeetHub

---
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    //memoization
+    int solve(vector<int>& piles, int n, int i, int m, int person, vector<vector<vector<int>>>& dp){
+        if(i >= n)
+            return 0;
+
+        if(dp[person][i][m] != -1) return dp[person][i][m];
+        int stones = 0;
+        int result;
+        if(person == 1) result = INT_MIN;
+        else result = INT_MAX;
+
+        int max_x = min(2*m, n - i);
+
+        for(int x = 1; x <= max_x; x++){
+            stones += piles[i + x - 1];
+
+            //when its your turn, do your best
+            if(person == 1){ //Alice's turn
+                result = max(result, stones + solve(piles, n, i+x, max(m, x), 0, dp));
+            }
+            //when its opponent's turn, expect the worst
+            else{//Bob's turn
+                result = min(result, solve(piles, n, i+x, max(m, x), 1, dp));
+            }
+        }
+        
+        return dp[person][i][m] = result;
+    }
+    int stoneGameII(vector<int>& piles) {
+        int i = 0;
+        int n = piles.size();
+        int m = 1;
+        int person = 1; //person = 1 means Alice and person = 2 means Bob
+        vector<vector<vector<int>>> dp(2, vector<vector<int>> (n+1, vector<int> (n+1, -1)));
+        return solve(piles, n, i, m, person, dp);
+    }
+};
