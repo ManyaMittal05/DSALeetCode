f91f7eabe090d30e99bcdfec833f2fa6263f1c18
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-22 20:24:34 +0530

    Time: 0 ms (100%), Space: 7.6 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int findComplement(int num) {
+        if (num == 0) return 1;
         
-        string newNum = "";
-        while(num > 0){
-            int rem = num % 2;
-            num = num / 2;
-            if(rem == 1) newNum += '0';
-            else newNum += '1';
+        unsigned int mask = ~0;
+        
+        while (num & mask) {
+            mask <<= 1;
         }
         
-        int ans = 0;
-        for(int i = 0; i<newNum.length(); i++){
-            if(newNum[i] == '1'){
-                ans += pow(2, i);
-            }
-        }
-
-        return ans;
+        return ~mask & ~num;
     }
 };
