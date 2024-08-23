f20070d40fbb7fe7f923be2decc9225a2d937700
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-23 09:42:07 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.44%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
-    string reduceFraction(string frac){
-        int i = 0;
-        string num = "", den = "";
+    pair<int ,int> reduceFraction(int numerator, int denominator){
 
-        while(frac[i] != '/'){
-            num += frac[i];
-            i++;
-        }
-
-        while(i < frac.length()){
-            if(frac[i] == '/'){
-                i++;
-                continue;
-            }
-            den += frac[i];
-            i++;
-        }
-
-        int numerator = stoi(num), denominator = stoi(den);
-        if(numerator == 0) return "0/1";
+        if(numerator == 0) return {0,1};
 
         int range = abs(denominator);
         if (abs(numerator) < abs(denominator)) range = abs(numerator);
@@ -31,10 +14,8 @@ public:
             }
         }
 
-        num = to_string(numerator);
-        den = to_string(denominator);
-
-        return (num + "/" + den);
+        
+        return {numerator, denominator};
     }
     string fractionAddition(string expression) {
         if(isdigit(expression[0])){
@@ -43,63 +24,33 @@ public:
         stack<string> st;
         
         int i = 0;
+        int currNum = 0, currDen = 1;
         while( i < expression.size()){
-            string val = "";
+            string num = "", den = "";
             if(expression[i] == '-' || expression[i] == '+'){
-                val += expression[i];
+                num += expression[i];
                 i++;
+                while(i < expression.size() && expression[i] != '/'){
+                    num += expression[i];
+                    i++;
+                }
                 while(i < expression.size() && expression[i] != '+' && expression[i] != '-'){
-                    val += expression[i];
+                    if(expression[i] == '/'){
+                        i++;
+                    }
+                    den += expression[i];
                     i++;
                 }
-                st.push(val);
+                
+                int n = stoi(num), d = stoi(den);
+                n = (n*currDen) + (currNum*d);
+                d = (d * currDen);
+                pair<int, int> p = reduceFraction(n, d);
+                currNum = p.first;
+                currDen = p.second;
             }
         }
 
-        while(st.size() != 1){
-            string frac1 = st.top();
-            st.pop();
-            string frac2 = st.top();
-            st.pop();
-            int i = 0, j = 0;
-
-            string num1 = "", den1 = "", num2 = "", den2 = "";
-            while(frac1[i] != '/'){
-                num1 += frac1[i];
-                i++;
-            }
-
-            while(i < frac1.length()){
-                if(frac1[i] == '/') {
-                    i++;
-                    continue;
-                }
-                den1 += frac1[i];
-                i++;
-            }
-
-            while(frac2[j] != '/'){
-                num2 += frac2[j];
-                j++;
-            }
-
-            while(j < frac2.length()){
-                if(frac2[j] == '/'){
-                    j++;
-                    continue;
-                }
-                den2 += frac2[j];
-                j++;
-            }
-
-            int n1 = stoi(num1), n2 = stoi(num2), d1 = stoi(den1), d2 = stoi(den2);
-
-            int ansNum = (n1*d2) + (n2*d1);
-            int ansDen = (d1*d2);
-            string final = to_string(ansNum) + "/" + to_string(ansDen);
-            st.push(final);
-        }
-
-        return reduceFraction(st.top());
+        return (to_string(currNum) + "/" + to_string(currDen));
     }
 };
