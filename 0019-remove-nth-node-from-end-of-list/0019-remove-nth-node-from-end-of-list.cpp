ee44bde1306c9c6cf7f01bb6e0cd59f09350e4f6
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-26 10:28:15 +0530

    Time: 8 ms (12.84%), Space: 14.7 MB (28.29%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,38 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head){
-        if(!head || !head->next) return head;
-
-        ListNode* newHead = reverseList(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
-    }
-
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(!head->next) return NULL;
 
-        head = reverseList(head);
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        if(n == 1){
+        for(int i = 1; i<= n; i++){
+            fast = fast->next;
+        }
+
+        //we have to remove the head of the list
+        if(!fast){
             head = head->next;
-            head = reverseList(head);
             return head;
         }
 
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-        while(n != 1){
-            prev = temp;
-            temp = temp->next;
-            n--;
+        while(fast->next){
+            slow = slow->next;
+            fast = fast->next;
         }
 
-        prev->next = temp->next;
-        delete temp;
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
 
-        return reverseList(head);
+        return head;
+
     }
 };
