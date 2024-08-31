86e3509a73f87ad1d5182067e3c9b976df4ea51c
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-31 14:17:06 +0530

    Time: 2536 ms (5.26%), Space: 766.8 MB (6.88%) - LeetHub

--- 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
+++ 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
@@ -1,85 +1,82 @@
 class Solution {
 public:
-    using int2=pair<int, int>;// (weight, vertex)
-    int n, m;
-    vector<vector<int2>> adj;
-    vector<int> wild_edges;
-    inline void build_graph(vector<vector<int>>& edges){
-        for(int i=0; i<m; i++){
-            auto& e=edges[i];
-            int u=e[0], v=e[1], w=e[2];
-            if (w>0){
-            //    cout<<w<<",";
-                adj[u].emplace_back(w, v);
-                adj[v].emplace_back(w, u);
-            }
-            else {
-            //    cout<<"mut:"<<i<<",";
-                wild_edges.push_back(i);
+     typedef long long ll;
+    const int LARGE_VALUE = 2e9;
+    typedef pair<long, long> P;
+
+    ll DijkstraAlgo(vector<vector<int>>& edges, int n, int src, int dest) {
+        //make the graph excluing -1 et edges
+        unordered_map<ll, vector<pair<ll, ll>>> adj; //u -> (v, wt)
+
+        for(vector<int>& edge : edges) {
+            if(edge[2] != -1) {
+                int u  = edge[0];
+                int v  = edge[1];
+                int wt = edge[2];
+
+                adj[u].push_back({v, wt});
+                adj[v].push_back({u, wt});
             }
         }
-    }
-    
-    int dijkstra(int src, int des, vector<vector<int2>>& adj){
-        int dist[100];
-        fill(dist, dist+n, INT_MAX);
-        priority_queue<int2, vector<int2>, greater<int2>> pq;
-        pq.emplace(0, src);
-        dist[src]=0;
 
-        while (!pq.empty()) {
-            auto [d0, i] = pq.top();
+        priority_queue<P, vector<P>, greater<P>> pq;
+        vector<ll> result(n, INT_MAX); //result[i] = shortest path distance of src to ith node
+        //vector<bool> visited(n, false); //YOU DON"T NEED VISITED ARRAY BECAUSE ANYWAYS YOU WILL NOT VISIT SAME NODE AS IT's DISTANCE WILL BE INCREASED NEXT TIME DIJKSTRA YOU WILL SKIP IT
+
+        result[src] = 0; //src to src distance is 0
+        pq.push({0, src});
+
+        while(!pq.empty()) {
+            ll currDist = pq.top().first;
+            ll currNode = pq.top().second;
             pq.pop();
-            if (i == des) return d0;
-            for (auto& [d_next, j] : adj[i]) {
-                int new_d = d0 + d_next;
-                if (new_d < dist[j]) {
-                    dist[j] = new_d;
-                    pq.emplace(new_d, j);
+
+            /*We don't need visited vector in Dijkstra
+            if(visited[currNode] == true) {
+                continue;
+            }
+            visited[currNode] = true;
+            */
+
+            for(auto&[nbr, wt] : adj[currNode]) {
+                if(result[nbr] > currDist + wt) {
+                    result[nbr] = currDist + wt;
+                    pq.push({result[nbr], nbr});
                 }
             }
         }
-        return INT_MAX;
-    }
-    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
-        this->n=n;
-    //    cout<<n<<endl;
-        adj.resize(n);
-        m=edges.size();
-    //    cout<<"m="<<m<<endl;
-        build_graph(edges);
 
-        int dist=dijkstra(source, destination, adj);
-   //     cout<<"dist="<<dist<<endl;
-        if (dist< target) return {};
-        
-        for (auto i: wild_edges)
-            edges[i][2]=target+1;// weight= target+1
-        if (dist==target) return edges;
-        
-        for (auto i: wild_edges){
-            auto& new_edge=edges[i];
-            int u=new_edge[0], v=new_edge[1];
-            new_edge[2]=1;
-            adj[u].emplace_back(1, v);
-            adj[v].emplace_back(1, u);
-            dist=dijkstra(source, destination, adj);
-        //    cout<<"add edges["<<i<<"] dist="<<dist<<endl;
-            if (dist<=target) {
-                new_edge[2]+=target-dist;
-        //        cout<<"last i="<<i<<", w="<<edges[i][2]<<endl;
-                return edges; 
+        return result[dest];
+    }
+
+    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
+        ll currShortestDist = DijkstraAlgo(edges, n, source, destination);
+
+        if(currShortestDist < target) {
+            return {};
+        }
+
+        bool matchedTarget = (currShortestDist == target);
+
+        for(vector<int>& edge : edges) {
+            if(edge[2] == -1) {
+
+                edge[2] = (matchedTarget == true) ? LARGE_VALUE : 1; //assign lowest number i.e. 1
+
+                if(matchedTarget != true) {
+                    ll newShortestDist = DijkstraAlgo(edges, n, source, destination);
+
+                    if(newShortestDist <= target) {
+                        matchedTarget = true;
+                        edge[2] += (target - newShortestDist);
+                    }
+                }
             }
         }
-        return {};
+
+        if(matchedTarget == false) {
+            return {};
+        }
+        return edges;
     }
 };
-
-
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
