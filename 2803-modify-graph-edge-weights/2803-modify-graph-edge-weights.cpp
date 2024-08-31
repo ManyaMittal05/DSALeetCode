07cda3bc986d4006c816e73e02bd02844d2cb486
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-31 14:24:50 +0530

    Time: 2518 ms (5.26%), Space: 766.8 MB (6.88%) - LeetHub

--- 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
+++ 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-     typedef long long ll;
-    const int LARGE_VALUE = 2e9;
-    typedef pair<long, long> P;
+    typedef long long ll;
+    const int large_val = 2e9;
+    typedef pair<long long, long long> P;
 
-    ll DijkstraAlgo(vector<vector<int>>& edges, int n, int src, int dest) {
-        //make the graph excluing -1 et edges
-        unordered_map<ll, vector<pair<ll, ll>>> adj; //u -> (v, wt)
-
-        for(vector<int>& edge : edges) {
-            if(edge[2] != -1) {
-                int u  = edge[0];
-                int v  = edge[1];
+    ll Dijkstra(int n, vector<vector<int>>& edges, int source, int dest){
+        unordered_map<ll, vector<pair<ll, ll>>> adj;
+        for(auto& edge : edges){
+            if(edge[2] != -1){
+                int u = edge[0];
+                int v = edge[1];
                 int wt = edge[2];
 
                 adj[u].push_back({v, wt});
@@ -20,63 +18,60 @@ public:
         }
 
         priority_queue<P, vector<P>, greater<P>> pq;
-        vector<ll> result(n, INT_MAX); //result[i] = shortest path distance of src to ith node
-        //vector<bool> visited(n, false); //YOU DON"T NEED VISITED ARRAY BECAUSE ANYWAYS YOU WILL NOT VISIT SAME NODE AS IT's DISTANCE WILL BE INCREASED NEXT TIME DIJKSTRA YOU WILL SKIP IT
 
-        result[src] = 0; //src to src distance is 0
-        pq.push({0, src});
+        vector<ll> distance(n, INT_MAX);
 
-        while(!pq.empty()) {
+        distance[source] = 0;
+        pq.push({distance[source], source});
+
+        while(!pq.empty()){
             ll currDist = pq.top().first;
             ll currNode = pq.top().second;
             pq.pop();
 
-            /*We don't need visited vector in Dijkstra
-            if(visited[currNode] == true) {
-                continue;
-            }
-            visited[currNode] = true;
-            */
+            for(auto &[nbr, wt] : adj[currNode]){
 
-            for(auto&[nbr, wt] : adj[currNode]) {
-                if(result[nbr] > currDist + wt) {
-                    result[nbr] = currDist + wt;
-                    pq.push({result[nbr], nbr});
+                if(currDist + wt < distance[nbr]){
+                    distance[nbr] = currDist + wt;
+                    pq.push({distance[nbr], nbr});
                 }
             }
         }
-
-        return result[dest];
+        return distance[dest];
     }
 
     vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
-        ll currShortestDist = DijkstraAlgo(edges, n, source, destination);
+        //calculate the initial shortest path without considering -ve edge weights
+        ll currShortestDist = Dijkstra(n, edges, source, destination);
 
-        if(currShortestDist < target) {
+        //no possible modifcation can result in shorter path
+        if(currShortestDist < target){
             return {};
         }
 
-        bool matchedTarget = (currShortestDist == target);
+        bool isMatchedTarget = (currShortestDist == target);
 
-        for(vector<int>& edge : edges) {
-            if(edge[2] == -1) {
+        //now thw last case is where shortest path > target
+        //here we can modify the -ve edge weights to achieve the target value
+        
+        for(vector<int>& edge : edges){
+            if(edge[2] == -1){
+                edge[2] = (isMatchedTarget == true) ? large_val : 1;
 
-                edge[2] = (matchedTarget == true) ? LARGE_VALUE : 1; //assign lowest number i.e. 1
+                if(isMatchedTarget != true){
+                    ll newShortestDist = Dijkstra(n, edges, source, destination);
 
-                if(matchedTarget != true) {
-                    ll newShortestDist = DijkstraAlgo(edges, n, source, destination);
-
-                    if(newShortestDist <= target) {
-                        matchedTarget = true;
+                    if(newShortestDist <= target){
+                        isMatchedTarget = true;
                         edge[2] += (target - newShortestDist);
                     }
                 }
             }
         }
 
-        if(matchedTarget == false) {
+        if(isMatchedTarget == false)
             return {};
-        }
+
         return edges;
     }
 };
