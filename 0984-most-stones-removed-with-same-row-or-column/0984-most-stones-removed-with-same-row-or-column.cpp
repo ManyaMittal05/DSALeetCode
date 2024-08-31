a629b38aecc75a5415fbb910337aec0158c89e02
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-08-31 11:02:10 +0530

    Time: 180 ms (14.45%), Space: 18 MB (78.18%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    void dfs(int &n, int idx, vector<bool>&visited, vector<vector<int>>&stones)
-    {
-        visited[idx] = true;
-        for(int i = 0; i<n; i++)
-        {
-            if(!visited[i])
-            {
-                if(stones[idx][0] == stones[i][0])
-                    dfs(n, i, visited, stones);
+    //we will apply dfs algorithm here
+    //mark the stones with any coordinate same to current stone as visited
+    //we will be calling dfs on all the stones by checking if they are not visited 
+    //and will mark all the stones in same row and column as visited
+    //after each dfs call we will increment the count of valid stones
+    //number of stones removed = total stones - valid stones 
+    void dfs(int ind, int n, vector<vector<int>>& stones, vector<bool>& visited){
+        visited[ind] = true;
 
-                if(stones[idx][1] == stones[i][1])
-                    dfs(n, i, visited, stones);
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                if(stones[ind][0] == stones[i][0]){
+                    dfs(i, n, stones, visited);
+                }
+                if(stones[ind][1] == stones[i][1]){
+                    dfs(i, n, stones, visited);
+                }
             }
-        }
+        } 
     }
-    int removeStones(vector<vector<int>>& stones) 
-    {
-        int n = stones.size();
-        int group = 0;
 
-        vector<bool>visited(n);
-        for(int i = 0; i<n; i++)
-        {
-            if(!visited[i])
-            {
-                dfs(n, i, visited, stones);
-                group += 1;
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int valid = 0;
+        vector<bool> visited(n, false);
+
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                dfs(i, n, stones, visited);
+                valid++;
             }
         }
-        return n - group;
+
+        return n - valid;
     }
 };
