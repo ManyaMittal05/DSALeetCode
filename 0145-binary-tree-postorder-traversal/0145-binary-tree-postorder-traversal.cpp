a0baa37f52d51f677511b02b2a2aa6c37dbfa29f
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-09-13 13:22:22 +0530

    Time: 2 ms (41.76%), Space: 10.2 MB (72.33%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -16,24 +16,20 @@ public:
         if(!root) return ans;
 
         stack<TreeNode*> st1;
-        stack<TreeNode*> st2;
 
         st1.push(root);
 
         while(!st1.empty()){
             TreeNode* node = st1.top();
             st1.pop();
-            st2.push(node);
+            ans.push_back(node->val);
             if(node->left)
                 st1.push(node->left);
             if(node->right)
                 st1.push(node->right);
         }
 
-        while(!st2.empty()){
-            ans.push_back(st2.top()->val);
-            st2.pop();
-        }
+        reverse(ans.begin(), ans.end());
 
         return ans;
     }
