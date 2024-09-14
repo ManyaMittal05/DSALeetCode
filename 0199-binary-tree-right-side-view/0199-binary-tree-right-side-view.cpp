3b9fb8fd9b6a88c818c94bc118185cc662c1127e
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-09-14 22:31:12 +0530

    Time: 3 ms (56.89%), Space: 14.3 MB (65.41%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,26 +11,24 @@
  */
 class Solution {
 public:
+//we have to return rightmost node of each level in the tree
+    //for this we will use recursive preorder traversal technique
+    //but instead of Root, Left, Right ... we will traverse Root, Right, Left
+    //beacause we need rightmost node
+    //if we have to return the left view of the tree, then we must have done preorder in root,left,right manner only
+    //if the size of data structure == level, then insert the node->val in data structure
+    void preOrder(TreeNode* root, vector<int>& res, int level){
+        if(!root) return;
+
+        if(res.size() == level) res.push_back(root->val);
+        preOrder(root->right, res, level+1);
+        preOrder(root->left, res, level+1);
+    }
+
     vector<int> rightSideView(TreeNode* root) {
-        map<int, int> mp; // level, value
-        queue<pair<TreeNode*, int>> q;
-
-        if(root) q.push({root, 0});
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            TreeNode* node = it.first;
-            int level = it.second;
-            mp[level] = node->val;
-
-            if(node->left) q.push({node->left, level+1});
-            if(node->right) q.push({node->right, level+1});
-        }
-
-        vector<int> rightview;
-        for(auto [level, value] : mp){
-            rightview.push_back(value);
-        }
-        return rightview;
+        vector<int> result;
+        int level = 0;
+        preOrder(root, result, level);
+        return result;    
     }
 };
