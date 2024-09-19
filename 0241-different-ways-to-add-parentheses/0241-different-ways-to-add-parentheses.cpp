b7fa697931537bb12a1e2ee95e80a4ec4f43c489
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-09-19 10:16:05 +0530

    Time: 17 ms (10.46%), Space: 19.6 MB (5.07%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -3,6 +3,8 @@ public:
     vector<int> diffWaysToCompute(string exp) {
         vector<int> ans;
 
+        unordered_map<string, vector<int>> dp;
+
         for(int ind = 0; ind < exp.size(); ind++){
             char curr = exp[ind];
             if(curr == '+' || curr == '-' || curr == '*'){
@@ -12,8 +14,11 @@ public:
                 string str1 = exp.substr(0, ind);
                 string str2 = exp.substr(ind+1);
 
-                left = diffWaysToCompute(str1);
-                right = diffWaysToCompute(str2);
+                if(dp.find(str1) != dp.end()) return dp[str1];
+                else left = diffWaysToCompute(str1);
+
+                if(dp.find(str2) != dp.end()) return dp[str2];
+                else right = diffWaysToCompute(str2);
 
                 for(auto i : left){
                     for(auto j : right){
@@ -28,7 +33,7 @@ public:
         }
 
         if(ans.empty()) ans.push_back(stoi(exp));
-        return ans;
+        return dp[exp] = ans;
 
     }
 };
