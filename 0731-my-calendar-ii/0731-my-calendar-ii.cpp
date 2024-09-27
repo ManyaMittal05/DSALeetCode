331ee5c7e18b0a1fcd64612c9437d1cc0087a17f
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-09-27 12:25:25 +0530

    Time: 60 ms (97.6%), Space: 37.8 MB (99.06%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,40 @@
+class MyCalendarTwo {
+    vector<pair<int,int>> doubleBookings;
+    vector<pair<int,int>> overallBookings;
+public:
+    MyCalendarTwo() {
+        
+    }
+
+    bool checkOverlapped(int s1, int e1, int s2, int e2){
+        if(max(s1, s2) < min(e1, e2))
+            return true;
+        return false;
+    }
+    
+    bool book(int start, int end) {
+
+        //check if triple booking is being created
+        for(auto it : doubleBookings){
+            if(checkOverlapped(it.first, it.second, start, end))
+                return false; //triple booking detected
+        }
+
+        //check if double booking is being created
+        for(auto it : overallBookings){
+            if(checkOverlapped(it.first, it.second, start, end)){
+                int newstart = max(it.first, start);
+                int newend = min(it.second, end);
+                doubleBookings.push_back({newstart, newend});
+            }
+        }
+        overallBookings.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
