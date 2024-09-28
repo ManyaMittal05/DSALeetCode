6b3affe50d25f84159bc776519a64c535f6dd489
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-09-29 00:09:42 +0530

    Time: 0 ms (100%), Space: 12.8 MB (79.53%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void helper(vector<int> candidates, int i, int target, set<vector<int>>& st,
-                vector<int> arr) {
-        if (i >= candidates.size()) {
-            if (target == 0) {
-                st.insert(arr);
-            }
+    void helper(vector<int>& candidates, int i, int target,
+                vector<vector<int>>& ans, vector<int>& arr) {
+        if (target == 0) {
+            ans.push_back(arr);
             return;
         }
 
-        helper(candidates, i+1, target, st, arr);
-
-        if (target - candidates[i] >= 0) {
-            arr.push_back(candidates[i]);
-            helper(candidates, i + 1, target - candidates[i], st, arr);
+        for (int index = i; index < candidates.size(); index++) {
+            if (index > i && candidates[index] == candidates[index - 1])
+                continue;
+            if (candidates[index] > target)
+                break;
+            arr.push_back(candidates[index]);
+            helper(candidates, index + 1, target - candidates[index], ans, arr);
+            arr.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        set<vector<int>> st;
+        vector<vector<int>> ans;
         vector<int> arr;
-        helper(candidates, 0, target, st, arr);
-        vector<vector<int>> ans(st.begin(), st.end());
+        helper(candidates, 0, target, ans, arr);
         return ans;
     }
 };
