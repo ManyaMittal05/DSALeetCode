5f48d1f7114a5edb6ba357fa4f065a0111b77013
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-10-09 15:40:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> set;
-        set.insert(wordList.begin(), wordList.end());
+        unordered_set<string> set(wordList.begin(), wordList.end());
         
         if(set.find(endWord) == set.end())
             return 0;
 
         set.erase(beginWord);
-
         queue<pair<string, int>> q;
         q.push({beginWord, 1});
+        int ans = 0;
 
         while(!q.empty()){
-            pair<string, int> ele = q.front();
-            string word = ele.first;
-            int count = ele.second;
+            auto p = q.front();
+            string word = p.first;
+            int count = p.second;
             q.pop();
-            
-            for(int i = 0; i<word.size(); i++){
+
+            for(int i = 0; i < word.length(); i++){
                 string w = word;
                 for(char ch = 'a'; ch <= 'z'; ch++){
                     w[i] = ch;
                     if(set.find(w) != set.end()){
-                        if(w == endWord){
-                            return count+1;
-                        }
-                        q.push({w, count+1});
+                        q.push({w, count + 1});
+                        ans = max(ans, count + 1);
                         set.erase(w);
                     }
                 }
             }
         }
-        return 0;
+        return ans;
     }
 };
