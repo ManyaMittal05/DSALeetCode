910a3c04ae307a9497719797a81363303bddbd3a
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-10-09 12:23:35 +0530

    Time: 249 ms (5.01%), Space: 279.9 MB (5.08%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,59 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    void dfs(vector<vector<char>> mat, int m, int n, int i, int j, vector<vector<bool>>& visited){
+        if(i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || mat[i][j] == 'X')
+            return;
 
-    void dfs(int i, int j, vector<vector<bool>>& visited, vector<vector<char>>& board){
         visited[i][j] = true;
 
-        for(auto& d : directions){
+        for(auto d : dir){
             int new_i = i + d[0];
             int new_j = j + d[1];
-            
-            if(new_i >= 0 && new_i < board.size() && new_j >= 0 && new_j < board[0].size()){
-                if(board[new_i][new_j] == 'O' && !visited[new_i][new_j])
-                    dfs(new_i, new_j, visited, board);
-            }
-            
+
+            dfs(mat, m, n , new_i, new_j, visited);
         }
     }
 
-    void solve(vector<vector<char>>& board) {
-        int m = board.size(), n = board[0].size();
+    void solve(vector<vector<char>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
         vector<vector<bool>> visited(m, vector<bool> (n, false));
 
-        //for upper boundary row
-        for(int j = 0; j < n; j++){
-            if(board[0][j] == 'O' && !visited[0][j]){
-                dfs(0, j, visited, board);
-            }
-        }
-
-        //for right boundary column
+        //for vertical boundaries
         for(int i = 0; i < m; i++){
-            if(board[i][n-1] == 'O' && !visited[i][n-1]){
-                dfs(i, n-1, visited, board);
+            if(!visited[i][0] && mat[i][0] == 'O'){
+                dfs(mat, m, n, i, 0, visited);
+            }
+            if(!visited[i][n-1] && mat[i][n-1] == 'O'){
+                dfs(mat, m, n, i, n-1, visited);
             }
         }
 
-        //for down boundary row
+        //for horizontal boundaries
         for(int j = 0; j < n; j++){
-            if(board[m-1][j] == 'O' && !visited[m-1][j]){
-                dfs(m-1, j, visited, board);
-            }
+            if(!visited[0][j] && mat[0][j] == 'O')
+                dfs(mat, m, n, 0, j, visited);
+            if(!visited[m-1][j] && mat[m-1][j] == 'O')
+                dfs(mat, m, n, m - 1, j, visited);
         }
 
-        //for left boundary column
+
         for(int i = 0; i < m; i++){
-            if(board[i][0] == 'O' && !visited[i][0]){
-                dfs(i, 0, visited, board);
-            }
-        }
-
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(visited[i][j] == false){
-                    board[i][j] = 'X';
-                }
+            for(int j = 0; j < n; j++){
+                if(!visited[i][j])
+                    mat[i][j] = 'X';
             }
         }
     }
