f2f06d86f19f01630b664bc089113df9b5f6267a
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-10-09 13:57:37 +0530

    Time: 126 ms (16.32%), Space: 43.9 MB (16.36%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,46 +1,61 @@
 class Solution {
 public:
-vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-    void dfs(vector<vector<int>>& mat, int m, int n, int i, int j, vector<vector<bool>>& visited){
-        if(i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || mat[i][j] == 0)
-            return;
 
-        visited[i][j] = true;
-        mat[i][j] = 0;
-
-        for(auto d : dir){
-            int new_i = i + d[0];
-            int new_j = j + d[1];
-
-            dfs(mat, m, n , new_i, new_j, visited);
-        }
-    }
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<bool>> visited(m, vector<bool> (n , false));
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
 
-        for(int i = 0; i < m; i++){
-            if(!visited[i][0] && grid[i][0] == 1)
-                dfs(grid, m, n, i, 0, visited);
-            if(!visited[i][n-1] && grid[i][n-1] == 1)
-                dfs(grid, m, n, i , n-1, visited);
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; i++) {
+            if (!visited[i][0] && grid[i][0] == 1){
+                q.push({i, 0});
+                visited[i][0] = true;
+            }
+            if (!visited[i][n - 1] && grid[i][n - 1] == 1){
+                q.push({i, n - 1});
+                visited[i][n-1] = true;
+            }
         }
 
-        for(int j = 0; j < n; j++){
-            if(!visited[0][j] && grid[0][j] == 1)
-                dfs(grid, m, n, 0, j, visited);
-            if(!visited[m-1][j] && grid[m-1][j] == 1)
-                dfs(grid, m, n, m-1, j, visited);
+        for (int j = 0; j < n; j++) {
+            if (!visited[0][j] && grid[0][j] == 1){
+                q.push({0, j});
+                visited[0][j] = true;
+            }
+            if (!visited[m - 1][j] && grid[m - 1][j] == 1){
+                q.push({m - 1, j});
+                visited[m-1][j] = true;
+            }
+        }
+
+        while (!q.empty()) {
+            auto p = q.front();
+            int i = p.first;
+            int j = p.second;
+            q.pop();
+
+            for (auto d : dir) {
+                int new_i = i + d[0];
+                int new_j = j + d[1];
+
+                if (new_i >= 0 && new_i < m && new_j >= 0 && new_j < n &&
+                    !visited[new_i][new_j] && grid[new_i][new_j] == 1){
+                        visited[new_i][new_j] = true;
+                        q.push({new_i, new_j});
+                    }
+            }
         }
 
         int count = 0;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j])
                     count++;
             }
         }
