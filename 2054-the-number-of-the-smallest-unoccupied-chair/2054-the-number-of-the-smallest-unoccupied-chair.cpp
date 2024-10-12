1dbde01e131c1a9f7d05a28fe5edc300582ea30f
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-10-12 11:34:31 +0530

    Time: 140 ms (72.7%), Space: 59 MB (81.91%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
+    typedef pair<int, int> P;
+
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        vector<int> chairs(n, -1);
-
-        int targetFrdArrTime = times[targetFriend][0]; //arrival times are distinct
+        priority_queue<P, vector<P>, greater<P>> occupied; //departure time, chair
+        priority_queue<int, vector<int>, greater<int>> freeChair;
+        int targetArr = times[targetFriend][0];
 
         sort(times.begin(), times.end());
+
+        int chair = 0;
+        if(targetArr == times[0][0]) return 0;
 
         for(auto time : times){
             int arrTime = time[0];
             int deptTime = time[1];
 
-            for(int i = 0; i < n; i++){
-                if(chairs[i] <= arrTime){
-                    chairs[i] = deptTime;
+            while(!occupied.empty() && occupied.top().first <= arrTime){
+                freeChair.push(occupied.top().second);
+                occupied.pop();
+            }
 
-                    if(targetFrdArrTime == arrTime)
-                        return i;
-                    break;
-                }
+            if(freeChair.empty()){
+                occupied.push({deptTime, chair});
+
+                if(arrTime == targetArr)
+                    return chair;
+                chair++;
+            }
+            else{
+                int leastAvailChair = freeChair.top();
+                if(arrTime == targetArr)
+                    return leastAvailChair;
+                occupied.push({deptTime, leastAvailChair});
+                freeChair.pop();
             }
         }
         return -1;
