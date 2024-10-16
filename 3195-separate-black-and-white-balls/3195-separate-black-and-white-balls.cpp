611db22ddbfd3c0a8fb0803bd5458e53a048052c
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-10-16 16:54:12 +0530

    Time: 23 ms (94.6%), Space: 15.7 MB (14.04%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long swap = 0;
-        int black = 0; 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '0') 
-                swap += (long long) black; 
-            else
-                black++; 
+        long long ans = 0, oneCount = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == '1') oneCount++;
+            else{
+                ans += oneCount;
+            }
         }
-        return swap;
+        return ans;
     }
 };
