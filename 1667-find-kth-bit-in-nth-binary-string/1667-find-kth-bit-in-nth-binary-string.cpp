9337f94550d5c848721c158bf71d464f853fa06b
Manya Mittal <72355681+ManyaMittal05@users.noreply.github.com>
2024-10-19 11:27:04 +0530

    Time: 43 ms (29.35%), Space: 58.7 MB (42.32%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -5,6 +5,7 @@ public:
             if(s[i] == '0') s[i] = '1';
             else s[i] = '0';
         }
+        reverse(s.begin(), s.end());
         return s;
     }
 
@@ -13,9 +14,8 @@ public:
         s[1] = "0";
 
         for(int i = 2; i < n + 1; i++){
-            string inverted = invert(s[i-1]);
-            reverse(inverted.begin(), inverted.end());
-            s[i] = s[i-1] + "1" + inverted;
+            string invertedAndRev = invert(s[i-1]);
+            s[i] = s[i-1] + "1" + invertedAndRev;
         }
         return s[n][k-1];
     }
